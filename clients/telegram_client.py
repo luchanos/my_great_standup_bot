bd975aef027716b885c701d9127b08677b138552
luchanos <Forgotten1000>
2022-08-24 00:20:13 +0400

    init

--- clients/telegram_client.py
+++ clients/telegram_client.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 import requests
 
 
@@ -8,35 +6,20 @@ class TelegramClient:
         self.token = token
         self.base_url = base_url
 
-    @staticmethod
-    def prepare_data_and_params(data: Union[None, dict], params: Union[None, dict]) -> tuple:
-        if params is None:
-            params = {}
-        if data is None:
-            data = {}
-        return params, data
-
     def prepare_url(self, method: str):
         result_url = f"{self.base_url}/bot{self.token}/"
-        if method != "":
+        if method is not None:
             result_url += method
         return result_url
 
-    def post(self, method: str = "", params: dict = None, data: dict = None):
-        params, data = self.prepare_data_and_params(params=params, data=data)
+    def post(self, method: str = None, params: dict = None, data: dict = None):
         url = self.prepare_url(method)
         resp = requests.post(url, params=params, data=data)
         return resp.json()
 
-    def get(self, method: str = "", params: dict = None, data: dict = None):
-        params, data = self.prepare_data_and_params(params=params, data=data)
-        url = self.prepare_url(method)
-        resp = requests.get(url, params=params, data=data)
-        return resp.json()
 
-
-token = "5499043180:AAEmbgJJ5shiKCQ9KnvN5S-2yAfw3PhBVuU"
-telegram_client = TelegramClient(token, base_url="https://api.telegram.org")
-my_params = {"chat_id": 362857450, "text": "sampleTEXT"}
-my_data = {"chat_id": 362857450, "text": "sampleTEXT"}
-telegram_client.post(method="sendMessage", params=my_params, data=my_data)
+if __name__ == "__main__":
+    token = "5499043180:AAEmbgJJ5shiKCQ9KnvN5S-2yAfw3PhBVuU"
+    telegram_client = TelegramClient(token=token, base_url="https://api.telegram.org")
+    my_params = {"chat_id": 362857450, "text": "sampleTEXT"}
+    print(telegram_client.post(method="sendMessage", params=my_params))
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import telebot
 from telebot.types import Message
 import json
-import requests
 from datetime import datetime
 from envparse import Env
-
+from clients.telegram_client import TelegramClient
 
 env = Env()
 TOKEN = env.str("TOKEN")
 ADMIN_CHAT_ID = env.int("ADMIN_CHAT_ID")
 
 
-bot_client = telebot.TeleBot(token=TOKEN)
+class MyBot(telebot.TeleBot):
+    def __init__(self, telegram_client: TelegramClient, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.telegram_client = telegram_client
 
 
-@bot_client.message_handler(commands=["start"])
+telegram_client = TelegramClient(token=TOKEN, base_url="https://api.telegram.org")
+bot = MyBot(token=TOKEN, telegram_client=telegram_client)
+
+
+@bot.message_handler(commands=["start"])
 def start(message: Message):
     with open("users.json", "r") as f_o:
         data_from_json = json.load(f_o)
@@ -27,25 +33,29 @@ def start(message: Message):
 
     with open("users.json", "w") as f_o:
         json.dump(data_from_json, f_o, indent=4, ensure_ascii=False)
-    bot_client.reply_to(message=message, text=str(f"Вы зарегистрированы: {username}. "
+    bot.reply_to(message=message, text=str(f"Вы зарегистрированы: {username}. "
                                                   f"Ваш user_id: {user_id}"))
 
 
 def handle_standup_speech(message: Message):
-    bot_client.reply_to(message, "Спасибо большое! Желаю успехов и хорошего дня!")
+    bot.reply_to(message, "Спасибо большое! Желаю успехов и хорошего дня!")
 
 
-@bot_client.message_handler(commands=["say_standup_speech"])
+@bot.message_handler(commands=["say_standup_speech"])
 def say_standup_speech(message: Message):
-    bot_client.reply_to(message, text="Привет! Чем ты занимался вчера?"
+    bot.reply_to(message, text="Привет! Чем ты занимался вчера?"
                                       "Что будешь делать сегодня?"
                                       "Какие есть трудности?")
-    bot_client.register_next_step_handler(message, handle_standup_speech)
+    bot.register_next_step_handler(message, handle_standup_speech)
+
+
+def create_err_message(err: Exception) -> str:
+    return f"{datetime.now()} ::: {err.__class__} ::: {err}"
 
 
 while True:
     try:
-        bot_client.polling()
+        bot.polling()
     except Exception as err:
-        requests.post(f"https://api.telegram.org/bot5499043180:AAEmbgJJ5shiKCQ9KnvN5S-2yAfw3PhBVuU"
-                      f"/sendMessage?chat_id=362857450&text={datetime.now()} ::: {err.__class__} ::: {err}")
+        bot.telegram_client.post(method="sendMessage", params={"text": create_err_message(err),
+                                                               "chat_id": ADMIN_CHAT_ID})
