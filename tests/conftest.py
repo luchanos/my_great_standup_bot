5020b22dd902c0a718b760ab62adf2a6bce0c5b8
luchanos <Forgotten1000>
2022-12-20 22:46:17 +0300

    init

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,31 @@ def db_connection():
     return sqlite3.connect(TEST_DB_URL)
 
 
+@pytest.fixture(scope="session", autouse=True)
+def create_moderate_tables(db_connection):
+    """Фикстура для создания и удаления таблиц"""
+    create_user_table_query = """
+CREATE TABLE "user" (
+id serial NOT NULL,
+"name" varchar NOT NULL,
+surname varchar NOT NULL,
+email varchar NOT NULL,
+hashed_password varchar NOT NULL,
+is_active bool NULL,
+CONSTRAINT user_pkey PRIMARY KEY (id)
+);
+"""
+
+    drop_user_table_query = """
+DROP TABLE users;
+"""
+    db_connection.execute(create_user_table_query)
+    db_connection.commit()
+    yield
+    db_connection.execute(drop_user_table_query)
+    db_connection.commit()
+
+
 @pytest.fixture(scope="function", autouse=True)  # будет вызываться автоматически перед началом запуска нового теста
 def clean_database(db_connection):  # передаем на вход результат работы другой фикстуры
     """Фикстура для очищения базы от данных до прогона теста"""
