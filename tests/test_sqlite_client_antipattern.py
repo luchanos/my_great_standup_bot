693c815b72ce122324d02702e61519035249ce5f
luchanos <Forgotten1000>
2022-12-22 00:56:16 +0300

    init

---
+++ tests/test_sqlite_client_antipattern.py
@@ -0,0 +1,101 @@
+import sqlite3
+
+from clients.sqlite3_client import SQLiteClient
+from tests.conftest import TEST_DB_URL
+
+
+# запрос для создания таблицы
+CREATE_USER_TABLE_QUERY = """
+CREATE TABLE IF NOT EXISTS users (
+user_id serial NOT NULL,
+username varchar NOT NULL,
+chat_id integer NOT NULL
+);
+"""
+
+# запрос для спиливания таблиц
+DROP_USER_TABLE_QUERY = """
+DROP TABLE users;
+"""
+
+def test_write_to_db_by_command():
+    # для начала создаём для тестирования этого отдельный коннект к БД, чтобы всё было независимо.
+    # запускаем команду для создания таблиц
+    db_connection = sqlite3.connect(TEST_DB_URL)
+    cursor = db_connection.cursor()
+    db_connection.execute(CREATE_USER_TABLE_QUERY)
+    db_connection.commit()
+
+
+    # команда, которая будет использоваться для тестирования клиента
+    command = """
+    INSERT INTO users (user_id, username, chat_id) VALUES (?, ?, ?);
+    """
+
+    # тестовые данные
+    user_id = 1
+    username = "luchanos"
+    chat_id = 123123
+
+    # создаём клиент, который мы будем тестировать
+    client = SQLiteClient(TEST_DB_URL)
+    client.create_conn()
+
+    # запускаем команду на запись в базу
+    client.execute_command(command, (user_id, username, chat_id))
+
+    # после этого ожидается, что в базе отобразится записанная строка и нам надо в этом убедиться.
+    # запускаем команду на чтение строк из базы
+    cursor.execute("""SELECT * FROM users;""")
+    users = cursor.fetchall()
+
+    # проверяем, что в базу действительно всё прописалось
+    assert len(users) == 1
+    user = users[0]
+    assert user[0] == user_id
+    assert user[1] == username
+    assert user[2] == chat_id
+
+    # удаляем таблицу
+    db_connection.execute(DROP_USER_TABLE_QUERY)
+    db_connection.commit()
+
+
+def test_read_from_db_by_client():
+    # для начала создаём для тестирования этого отдельный коннект к БД, чтобы всё было независимо.
+    # запускаем команду для создания таблиц
+    db_connection = sqlite3.connect(TEST_DB_URL)
+    db_connection.execute(CREATE_USER_TABLE_QUERY)
+    db_connection.commit()
+
+    # тестовые данные
+    user_id = 1
+    username = "luchanos"
+    chat_id = 123123
+
+    # создадим в базе строку, которую потом будем читать с помощью тестируемого клиента
+    db_connection = sqlite3.connect(TEST_DB_URL)
+    db_connection.execute("""INSERT INTO users (user_id, username, chat_id) VALUES (?, ?, ?);""",
+                          (user_id, username, chat_id))
+    db_connection.commit()
+
+    # команда для передачи в клиент, который мы будем тестировать
+    command = """
+    SELECT * FROM users;
+    """
+
+    # создаем клиент, который будем тестировать
+    client = SQLiteClient(TEST_DB_URL)
+    client.create_conn()
+    users = client.execute_select_command(command)
+
+    # проверяем, что достали из базы желаемые значения
+    assert len(users) == 1
+    user = users[0]
+    assert user[0] == user_id
+    assert user[1] == username
+    assert user[2] == chat_id
+
+    # удаляем таблицу
+    db_connection.execute(DROP_USER_TABLE_QUERY)
+    db_connection.commit()
